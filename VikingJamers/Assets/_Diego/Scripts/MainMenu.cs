fd18b24488d97168fbd764989650b89f772d1fbe
DiegoGR <diguti85@gmail.com>
2019-04-06 19:42:28 +0200

    creditos

--- VikingJamers/Assets/_Diego/Scripts/MainMenu.cs
+++ VikingJamers/Assets/_Diego/Scripts/MainMenu.cs
@@ -7,13 +7,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button btn_play;
-    public Button btn_credits;
     public Button btn_exit;
 
     void Start()
     {
         btn_play.onClick.AddListener(() => GoToLevel(1));
-        btn_credits.onClick.AddListener(() => GoToLevel(99));
         btn_exit.onClick.AddListener(() => Exit());
     }
 
