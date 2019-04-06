d10832f7889a58e9fdd5d861be8998e74e151e72
pirri1721 <pirri1721@gmail.com>
2019-04-06 04:17:12 +0200

    CameraShipController

---
+++ VikingJamers/Assets/_Pirri/Scripts/CameraShipController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShipController : MonoBehaviour
+{
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =  Vector3.Lerp(transform.position, Target.transform.position + Target.transform.up, Time.deltaTime);
+    }
+}
